a36b0d1c18f1f0bf2f4a3fe99c7a4f93bb543eae
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2022-12-09 12:20:16 +0100

    Add files via upload

--- Arduino/bluetooth_dual_mode/bluetooth_dual_mode.ino
+++ Arduino/bluetooth_dual_mode/bluetooth_dual_mode.ino
@@ -1,10 +1,9 @@
 #include <SoftwareSerial.h>   //Software Serial Port
 
-#define RxD A10
-#define TxD A11
+#define RxD A10 //Sur Uno broche D2 et sur Mega broche A10 
+#define TxD A11 //Sur Uno broche D3 et sur Mega broche A11 
 #define vitesse_arduino 9600
 #define vitesse_bluetooth 115200
-
 SoftwareSerial blueToothSerial(RxD,TxD);
 
 String recvChar;
@@ -23,36 +22,36 @@ void setup()
   blueToothSerial.flush();
   blueToothSerial.print("AT");
   Serial.println(blueToothSerial.readString());
-//  blueToothSerial.print("AT+NAMEEDR.dual.01"); //Nom Module
-//  Serial.print("Nom du module (EDR) : ");
-//  Serial.println(blueToothSerial.readString());
-//  blueToothSerial.print("AT+NAMBBLE.dual.01"); //Nom Module
-//  Serial.print("Nom du module (BLE) : ");
-//  Serial.println(blueToothSerial.readString());
-//  blueToothSerial.print("AT+SCAN?"); //Nom Module
-//  Serial.print("SCAN : ");
-//  Serial.println(blueToothSerial.readString());
-//  Serial.print("Vitesse de transmission : ");
-//  blueToothSerial.print("AT+BAUD?"); // Vitesse Transmission
-//  Serial.println(blueToothSerial.readString());
-//  Serial.print("Code Pin (EDR): ");
-//  blueToothSerial.print("AT+PINE?"); // Code PIN
-//  Serial.println(blueToothSerial.readString());
-//  Serial.print("Code Pin (BLE): ");
-//  blueToothSerial.print("AT+PINB?"); // Code PIN
-//  Serial.println(blueToothSerial.readString());
-//  Serial.print("Version : ");
-//  blueToothSerial.print("AT+VERS?"); // Version
-//  Serial.println(blueToothSerial.readString());
-//  Serial.print("Adresse MAC (EDR): ");
-//  blueToothSerial.print("AT+ADDE?"); // Température du module
-//  Serial.println(blueToothSerial.readString());
-//  Serial.print("Adresse MAC (BLE): ");
-//  blueToothSerial.print("AT+ADDB?"); // Température du module
-//  Serial.println(blueToothSerial.readString());
-//  Serial.print("Adresse DUAL : ");
-//  blueToothSerial.print("AT+DUAL?"); // Température du module
-//  Serial.println(blueToothSerial.readString());
+  blueToothSerial.print("AT+NAMEEDR.bluetooth10"); //Nom Module
+  Serial.print("Nom du module (EDR) : ");
+  Serial.println(blueToothSerial.readString());
+  blueToothSerial.print("AT+NAMBBLE.bluetooth10"); //Nom Module
+  Serial.print("Nom du module (BLE) : ");
+  Serial.println(blueToothSerial.readString());
+  blueToothSerial.print("AT+SCAN?"); //Nom Module
+  Serial.print("SCAN : ");
+  Serial.println(blueToothSerial.readString());
+  Serial.print("Vitesse de transmission : ");
+  blueToothSerial.print("AT+BAUD?"); // Vitesse Transmission
+  Serial.println(blueToothSerial.readString());
+  Serial.print("Code Pin (EDR): ");
+  blueToothSerial.print("AT+PINE?"); // Code PIN
+  Serial.println(blueToothSerial.readString());
+  Serial.print("Code Pin (BLE): ");
+  blueToothSerial.print("AT+PINB?"); // Code PIN
+  Serial.println(blueToothSerial.readString());
+  Serial.print("Version : ");
+  blueToothSerial.print("AT+VERS?"); // Version
+  Serial.println(blueToothSerial.readString());
+  Serial.print("Adresse MAC (EDR): ");
+  blueToothSerial.print("AT+ADDE?"); // Température du module
+  Serial.println(blueToothSerial.readString());
+  Serial.print("Adresse MAC (BLE): ");
+  blueToothSerial.print("AT+ADDB?"); // Température du module
+  Serial.println(blueToothSerial.readString());
+  Serial.print("Adresse DUAL : ");
+  blueToothSerial.print("AT+DUAL?"); // Température du module
+  Serial.println(blueToothSerial.readString());
   Serial.println("Fin setup");
   delay(2000);
   blueToothSerial.flush();
